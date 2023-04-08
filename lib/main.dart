02799097db3d02a84dce03217909cee3fa640c80
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-04-08 22:45:10 +0100

    use multiProvider to have products and cart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview.dart';
 import './screens/product_detail_screen.dart';
 
+import './providers/cart.dart';
 import './providers/products.dart';
 
 void main() => runApp(MyApp());
@@ -11,8 +12,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
